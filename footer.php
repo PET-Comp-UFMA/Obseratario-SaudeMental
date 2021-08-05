7f7caf38543b619a8964ac4d30498c3c01162144
jovijovi-john <jovijovi2019@hotmail.com>
2021-08-05 11:13:00 -0300

    modificacoes_footer

--- footer.php
+++ footer.php
@@ -12,6 +12,8 @@
       <li><img src="./assets/svg/email icon.svg" alt="Ícone email" loading="lazy"><p>atendimento@observatoriodesaudemental.com.br; cpdsobral2017@gmail.com</p></li>
       <li><img src="./assets/svg/phone_black_24dp.svg" alt="Ícone telefone" loading="lazy"><p class="numeros-contatos">(88)993584258; (88)3611-1164</p></li>
     </ul>
+    
+    <!-- aqui fica o botão de fale conosco -->
     <div class="contatos">
       <a class="contato" href="./contato.php">
         <img src="./assets/svg/comment icon.svg" alt="Ícone comentário" loading="lazy">Fale Conosco
