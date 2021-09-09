a8c4098badb65ae2e7f3790870bd3e75ad5a8e46
jovijovi-john <jovijovi2019@hotmail.com>
2021-09-09 11:55:48 -0300

    att-rodape

--- footer.php
+++ footer.php
@@ -10,7 +10,6 @@
     <h4>Contatos</h4>
     <ul>
       <li><img src="./assets/svg/email icon.svg" alt="Ícone email" loading="lazy"><p>atendimento@observatoriodesaudemental.com.br; cpdsobral2017@gmail.com</p></li>
-      <li><img src="./assets/svg/phone_black_24dp.svg" alt="Ícone telefone" loading="lazy"><p class="numeros-contatos">(88)993584258; (88)3611-1164</p></li>
     </ul>
     <div class="contatos">
       <a class="contato" href="./contato.php">
