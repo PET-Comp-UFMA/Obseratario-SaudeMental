b0c007a77cc5d3063f7394d3fa2f128e53ab9824
Luigi Dovera <luigidovera11@gmail.com>
2021-12-13 12:07:32 -0300

    att galeria de imagem

--- index.php
+++ index.php
@@ -84,16 +84,20 @@
       <div class="resumo-dexters">
         <div class="images">
           <figure class="gallery__item gallery__item--1">
-            <img src="./assets/images/photo-5.jpg" alt="Gallery image 1" loading="lazy" class="gallery__img">
+            <img src="./assets/images/photo-8.jpg" alt="Gallery image 1" loading="lazy" class="gallery__img">
           </figure>
           <figure class="gallery__item gallery__item--2">
-            <img src="./assets/images/photo-2.jpg" alt="Gallery image 2" loading="lazy" class="gallery__img">
+            <img src="./assets/images/photo-6.jpg" alt="Gallery image 2" loading="lazy" class="gallery__img">
           </figure>
           <figure class="gallery__item gallery__item--3">
-            <img src="./assets/images/photo-4.jpg" alt="Gallery image 3" loading="lazy" class="gallery__img">
+            <img src="./assets/images/photo-9.jpg" alt="Gallery image 3" loading="lazy" class="gallery__img">
           </figure>
           <figure class="gallery__item gallery__item--4">
-            <img src="./assets/images/photo-1.jpg" alt="Gallery image 4" loading="lazy" class="gallery__img">
+            <img src="./assets/images/photo-7.jpg" alt="Gallery image 4" loading="lazy" class="gallery__img">
+          </figure>
+          <figure class="gallery__item gallery__item--5">
+            <img src="./assets/images/photo-10.jpg" alt="Gallery image 4" loading="lazy" class="gallery__img"> 
+          </figure>
         </div>
         <div class="text-dexters">
           <h1>Conheça o <mark class="highlight">Observatório</mark></h1>
