7b33e34e4e47463ad1928ead1c7cddc59fa6ac33
Luigi Dovera <luigidovera11@gmail.com>
2021-12-14 10:20:02 -0300

    att footer

--- footer.php
+++ footer.php
@@ -1,21 +1,49 @@
 <footer>
-  <div class="logos-footer">
-    <a href="./index.php"><img src="./assets/images/logos/logo2.png" alt="Logo Observatório" loading="lazy"></a>
-    <a href="http://saude.sobral.ce.gov.br/politicas-sobre-drogas" target="_blank"><img src="./assets/images/logos/cpsds-texto.png" alt="Logo Sobral - Politícas sobre Drogas" loading="lazy" class="logo-observatorio">
-    <a href="" style="pointer-events: none;"><img src="./assets/images/logos/gesam-texto.png" alt="" loading="lazy"></a>
-    <a href="http://www.sobral.ce.gov.br" target="_blank"><img src="./assets/images/logos/sobral-texto.png" alt="Logo Prefeitura Sobral" loading="lazy"></a>
-    <a href="http://www.uvanet.br" target="_blank"><img src="./assets/images/logos/uva.png" alt="Logo Universidade do Vale do Acaraú" loading="lazy"></a>
-  </div>
-  <div class="infos">
-    <h4>Contatos</h4>
-    <div>
-        <img src="./assets/svg/email icon.svg" alt="Ícone email" loading="lazy">
-        <p>observatoriodesaudemental@gmail.com</p>
+    <div class="logos-footer">
+        <a href="./index.php"><img src="./assets/images/logos/logo2.png" alt="Logo Observatório" loading="lazy"></a>
+        <a href="http://saude.sobral.ce.gov.br/politicas-sobre-drogas" target="_blank"><img
+                src="./assets/images/logos/cpsds-texto.png" alt="Logo Sobral - Politícas sobre Drogas" loading="lazy"
+                class="logo-observatorio">
+            <a href="" style="pointer-events: none;"><img src="./assets/images/logos/gesam-texto.png" alt=""
+                    loading="lazy"></a>
+            <a href="http://www.sobral.ce.gov.br" target="_blank"><img src="./assets/images/logos/sobral-texto.png"
+                    alt="Logo Prefeitura Sobral" loading="lazy"></a>
+            <a href="http://www.uvanet.br" target="_blank"><img src="./assets/images/logos/uva.png"
+                    alt="Logo Universidade do Vale do Acaraú" loading="lazy"></a>
     </div>
-    <div class="contatos">
-      <a class="contato" href="./contato.php">
-        <img src="./assets/svg/comment icon.svg" alt="Ícone comentário" loading="lazy">Fale Conosco
-      </a>
+    <div class="infos">
+        <h4>Contatos</h4>
+        <div>
+            <ul>
+                <li>
+                    <img src="./assets/svg/email icon.svg" alt="Ícone email" loading="lazy">
+                    <p>observatoriodesaudemental@gmail.com</p>
+                </li>
+                <li>
+                    <img src="./assets/svg/instagram icon copy.svg" alt="Ícone Twitter" loading="lazy">
+                    <a href="https://www.instagram.com/observatoriodesaudemental_/">
+                        @ObservatorioSaudeMental_
+                    </a>
+                </li>
+                <li>
+                    <img src="./assets/svg/twitter icon copy.svg" alt="Ícone Twitter" loading="lazy">
+                    <a href="https://twitter.com/OBSERVATRIODES1?s=20">
+                        @OBSERVATRIODES1
+                    </a>
+                </li>
+                <li>
+                    <img src="./assets/svg/facebook icon copy.svg" alt="Ícone Twitter" loading="lazy">
+                    <a
+                        href="https://www.facebook.com/Observat%C3%B3rio-de-sa%C3%BAde-mental-e-pol%C3%ADticas-sobre-Drogas-103944485385765/">
+                        Observatório Saúde Mental
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="contatos">
+            <a class="contato" href="./contato.php">
+                <img src="./assets/svg/comment icon.svg" alt="Ícone comentário" loading="lazy">Fale Conosco
+            </a>
+        </div>
     </div>
-  </div>
 </footer>
